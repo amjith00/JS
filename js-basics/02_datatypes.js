b8e05b5a39e48432cbfe279a8e726d9dca4ff4c0
amjith00 <136812780+amjith00@users.noreply.github.com>
2025-03-27 10:58:43 +0000

    conversions

--- js-basics/02_datatypes.js
+++ js-basics/02_datatypes.js
@@ -2,6 +2,6 @@
 let a=40  //number type
 let b="amjith" //stringtype
 let c=true/ false //boolean type
-let d= null //called objeect type
+let d= null //called object type
 let e = undifined //undifined type
 
---
+++ js-basics/03_conversions.js
@@ -0,0 +1,11 @@
+// // Type conversions
+// let numbber = "10"
+
+// // let numberIn = boolean(number)
+// console.log(string(number))
+
+let a = "90";
+
+let numberIn = boolean(a)
+console.log(numberIn);
+
