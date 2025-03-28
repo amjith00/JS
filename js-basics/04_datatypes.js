87ca8db72c3ca297190ee5b3e621df545f882e42
amjith00 <136812780+amjith00@users.noreply.github.com>
2025-03-28 06:30:59 +0000

    datatypes

---
+++ js-basics/04_datatypes.js
@@ -0,0 +1,23 @@
+// primitive type
+const totalSpread = 240 //numbertype
+const  isLoggedIn = true//booleantype
+const native = null //nultype //result will be object type
+const mentionName; //undifined
+let name = "amjith" //string
+
+const bigNumber = 12345677889776n //BigInt
+
+// Referencetype(nonprimitive)
+// array
+const makers = ["tom", "cure","jamp"];//array type
+// object type
+let student = {
+    name : "amjith",
+    age : 20,
+    eligible : ture
+}
+// fuctiontype
+let functionName  = function(){
+    console.log("amjith");
+}
+
