293c668b5eca40335c2596cadfb5a2f13d94837f
amjith00 <amjith9900@gmail.com>
2025-03-29 16:52:25 +0530

    string

---
+++ js-basics/string.js
@@ -0,0 +1,6 @@
+const holder = "amjith";
+console.log(holder.length);
+console.log(holder.toUpperCase());
+console.log(holder.toLowerCase());
+console.log(holder.split(""));
+console.log(holder.charAt(2));
